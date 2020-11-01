e8ec00e32fd1f2b5b5396453146e3d2b8158032e
bona <bona373737@gmail.com>
2020-11-01 12:32:53 +0900

    MainActivity를 BaseActivity 기반의 구조로 변경(코드정리)

--- app/src/main/java/com/t/jicbangcopy_20201101/MainActivity.kt
+++ app/src/main/java/com/t/jicbangcopy_20201101/MainActivity.kt
@@ -3,9 +3,16 @@ package com.t.jicbangcopy_20201101
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    override fun setupEvents() {
+    }
+    override fun setValues() {
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
 }
